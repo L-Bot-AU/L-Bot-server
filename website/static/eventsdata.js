43b92b9e2e19669026406fdf93c26ace63fb0bd8
yellowsubmarine1447 <cyril.subramanian@gmail.com>
2021-09-06 15:36:46 +1000

    Added events toggle
    Added Ryan's component, which is the events toggle

--- website/static/eventsdata.js
+++ website/static/eventsdata.js
@@ -1,4 +1,5 @@
 sio.on("events", data => {
+	console.log(data);
 	//data = [{"text": "foo bar", "impact": "high", "library": "jnr"}, {"text": "baz", "impact": "low", "library": "snr"}]
 	var ul = document.getElementById("list");
 	ul.innerHTML = "";
@@ -6,7 +7,15 @@ sio.on("events", data => {
 		var ul = document.getElementById("list");
 		var li = document.createElement("li");
 		li.appendChild(document.createTextNode(value["text"]));
-		li.setAttribute("class", value["impact"] + "impact");
+		li.setAttribute("class", value["impact"] + "impact" + " " + value["library"] + "event");
 		ul.appendChild(li);
 	});
 });
+
+function toggleEvents(cb) {
+	console.log(cb);
+	var list = document.getElementsByClassName(cb.name);
+	for (var i = 0; i < list.length; i++) {
+		list[i].style.display = ["none", ""][cb.checked + 0];
+	}
+}
